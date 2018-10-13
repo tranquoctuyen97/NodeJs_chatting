a8589aed9d4159c42bcbba23ad3becbd54206120
tranquoctuyen97 <tuyentqpd02042@fpt.edu.vn>
2018-10-14 02:10:04 +0700

    create group block user

--- server/routes/block-api-router.js
+++ server/routes/block-api-router.js
@@ -8,4 +8,6 @@ module.exports = (app) => {
 
     app.route('/blocks')
         .post([Authentication.isAuth], blockController.createBlock);
+    app.route('/users/:id/block/:groupId')
+        .post([Authentication.isAuth], blockController.createGroupBlockUser);
 };
